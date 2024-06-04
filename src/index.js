01a57b5f57ca78b8b55afd690c7d3348f2f05435
Karlito14 <leiroz26@hotmail.com>
2024-06-04 18:52:40 +0200

    affichage des todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+// affichage des todo
 
+const form = document.querySelector('.form');
+const input = document.querySelector('input[type="text"]');
+const list = document.querySelector('#todo_list');
+
+const addTodo = () => {
+  let inputValue = input.value;
+  inputValue = inputValue[0].toUpperCase() + inputValue.slice(1)
+
+  const template = document.querySelector('#template');
+  const clone = template.content.cloneNode(true);
+  const pagraph = clone.querySelector('p');
+  pagraph.textContent = inputValue;
+
+  list.append(clone);
+  input.value = '';
+  input.focus();
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addTodo();
+});
